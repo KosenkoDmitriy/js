ee7ad64ffd601df05c965af2b34cf7c610292f22
Dmitry K <kosenkodmitryv@gmail.com>
2023-03-08 17:52:32 +0300

    useState hook

--- scalable_react_app/src/Components/OnInputChange.jsx
+++ scalable_react_app/src/Components/OnInputChange.jsx
@@ -6,7 +6,15 @@ const OnInputChange = () => {
         setData(event.target.value)
         console.log(data)
     }
-    return <input type='text' onChange={onChange} />
+    const [userName, setUserName] = useState<string>('')
+    // const onUserNameChange(name) {
+    //     setUserName(name)
+    //     console.log(name)
+    // }
+    return <div>
+        <input type='text' onChange={onChange} />
+        <input type='text' onChange={(e) => setUserName(e.target.value)} />
+    </div>
 }
 
 export { OnInputChange }
