0d58006ae748de72408cfe1f9edec96ec211d1a7
Dmitry K <kosenkodmitryv@gmail.com>
2023-03-11 15:30:09 +0300

    useCallback

--- scalable_react_app_ts/src/Components/ReactHooksComponent.tsx
+++ scalable_react_app_ts/src/Components/ReactHooksComponent.tsx
@@ -1,14 +1,35 @@
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 
 function UseMemoComponent() {
     const result = useMemo(() => {
         return 2 + 2
     }, [])
-    
+
+    const values = [9,2,7,4,6]
+    const sortedValues = () => {
+        console.log('sortedValues')
+        return values.sort().join(', ') 
+    }
+
+    const sortedValuesWithMemo = useMemo(() => {
+        console.log('sortedValuesWithMemo')
+        return values.sort().join(', ') 
+    }, [])
+
+    const sortedValuesWithCallback = useCallback(() => {
+        console.log('sortedValuesWithCallback')
+        return values.sort().join(', ') 
+    }, [])
+
     return (
         <>
             <h1>useMemo (react hook) </h1>
-            <p>2 + 2 = {result}</p>
+            <p>unsorted values: {values.join(', ')}</p>
+            <p>sorted values (): {sortedValues()} </p>
+            <p>sorted values (with memo): {sortedValuesWithMemo} </p>
+            <p>sorted values (with callback): {sortedValuesWithCallback()} </p>
+
+            <p>2 + 2 = {result}</p> 
             <p>1 + 3 = {result}</p>
         </>
     )
